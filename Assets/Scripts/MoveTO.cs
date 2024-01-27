4d7a2a81081a52e64daf34b89ebcc45a3fc6e39d
CSivira <carlos.csivira@gmail.com>
2024-01-27 12:25:33 -0400

    feat: Enemy PF

---
+++ Assets/Scripts/MoveTO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.AI;
+    
+public class MoveTo : MonoBehaviour {
+    void Start () {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = Target.instance.transform.position;
+    }
+}
