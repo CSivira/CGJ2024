eb94c3ec653e7b13147286a70888750aefa9b1cf
JRBarreraM <mrjrbarreram@gmail.com>
2024-01-27 12:08:51 -0400

    melchor player movement and bullet

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D theRb;
+
+    public float moveSpeed = 5f;
+
+    private Vector2 _moveInput;
+    private Vector2 _mouseInput;
+
+    public float mouseSensitivity = 1f;
+
+    public Camera viewCam;
+
+    public GameObject bulletImpact;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player movement
+        _moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector3 moveHorizontal = transform.up * -_moveInput.x;
+        Vector3 moveVertical = transform.right * _moveInput.y;
+
+        theRb.velocity = (moveHorizontal + moveVertical) * moveSpeed;
+        
+        //player view control
+        _mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z - _mouseInput.x);
+        viewCam.transform.localRotation = Quaternion.Euler(viewCam.transform.localRotation.eulerAngles + new Vector3(0f, _mouseInput.y, 0f));
+        
+        //shooting
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = viewCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                //Debug.Log("I'm looking at "+ hit.transform.name);
+                Instantiate(bulletImpact, hit.point, transform.rotation);
+            }
+            else
+            {
+                Debug.Log("I'm looking at nothing");
+            }
+        }
+    }
+}
